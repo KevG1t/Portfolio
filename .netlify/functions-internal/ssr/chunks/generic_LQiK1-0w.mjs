9a4a0e1e4e7b4dd33b2fa2aff6e0c82f425a07c7
Kevin Moreno <kevinantonioc13@gmail.com>
2024-03-13 23:19:47 -0600

    build

--- .netlify/functions-internal/ssr/chunks/generic_LQiK1-0w.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_DKl6OJZX.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_17SBPemC.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Cuw_E03M.mjs');
-
-export { page };
