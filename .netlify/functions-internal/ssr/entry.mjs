dc843acc497edf574a593ef5be7aa3c0a116347e
Kevin Moreno <kevinantonioc13@gmail.com>
2024-03-14 11:22:08 -0600

    move images

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,10 +1,10 @@
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_DCYmiUZS.mjs';
+import { manifest } from './manifest_XKQ_mIw6.mjs';
 import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./chunks/generic_lmuGh2IY.mjs');
-const _page1 = () => import('./chunks/index_Dto_q_Qb.mjs');
+const _page1 = () => import('./chunks/index_DzDUuwgJ.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/index.astro", _page1]
@@ -16,7 +16,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "b6d20077-e70f-48ec-b87a-b5d409cd18c3"
+    "middlewareSecret": "1b633ba3-28e8-4e53-a6a0-67bf34c9731c"
 };
 const _exports = serverEntrypointModule.createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
